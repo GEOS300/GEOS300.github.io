0b48a1809c225f8c4510a2a32650a49f72dfbc73
June-Skeeter <thruhikerflash@gmail.com>
2024-01-26 15:30:17 -0800

    Assignment 1

--- _Code/CopyCode.py
+++ _Code/CopyCode.py
@@ -1,78 +1,80 @@
 import os, shutil
 from distutils.dir_util import copy_tree
+import nbformat
 import re
-
-In_Dir = 'Assignments/'
-Write_Dir = '../AssignmentCode/'
-Out_Dir = 'docs/'
-for file in os.listdir(In_Dir):
-    mv = 0
-    if file.endswith(".html.md"):
-        with open(f"{In_Dir}/{file}",'r',encoding='utf-8') as T:
-            text = T.read()
-            text = text.replace('\n','~~~~~~')
-            A = re.findall(r'::: {.cell-output .cell-output-stdout}(.*?):::',text)
-            B = re.findall(r'::: {.cell-output .cell-output-stderr}(.*?)```(.*?)```',text)
-            C = re.findall(r'::: {.cell-output-display}(.*?):::',text)
-            for rem in [A,B,C]:
-                for i,a in enumerate(rem):
-                    if isinstance(a, tuple):
-                        for part in a:
-                            text=text.replace(part,'\n')
-                    else:
-                        text=text.replace(a,'\n')
-            text=text.replace('.r .cell-code','r')
-            text=text.replace('::: {.cell}','\n')
-            text=text.replace('::: {.cell-output .cell-output-stdout}','\n')
-            text=text.replace('::: {.cell-output .cell-output-stderr}','\n')
-            text=text.replace('::: {.cell-output-display}','\n')
-            text=text.replace(':::','\n')
-            text=text.replace('```~~~~~~```','\n')
-            text=text.replace('~~~~~~','\n')
-            
-        with open(Write_Dir+file.split(".html.md")[0]+'.rmd','w',encoding='utf-8') as out:
-            out.write(text)
-        os.remove(f"{In_Dir}/{file}")
-        print(f'Converted {file} to RMD')
-        mv = 1
-    elif file.endswith(".ipynb"):
-        shutil.move(In_Dir+file, Write_Dir+file)
-
-        mv = 1
-        print(f'Moved {file}')
-    if mv == 1:
-        print('Moving ',Out_Dir+In_Dir+file.split('.')[0]+'_files')
-        copy_tree(Out_Dir+In_Dir+file.split('.')[0]+'_files', Write_Dir+file.split('.')[0]+'_files')
-    
-
-# import pandas as pd
 
 # if not os.getenv("QUARTO_PROJECT_RENDER_ALL"):
 #     exit()
 
-# KeyDir = '../AnswerKeys'
+def R_rep(text):
+    text = text.replace('\n','~~~~~~')
+    A = re.findall(r'::: {.cell-output .cell-output-stdout}(.*?):::',text)
+    B = re.findall(r'::: {.cell-output .cell-output-stderr}(.*?)```(.*?)```',text)
+    C = re.findall(r'::: {.cell-output-display}(.*?):::',text)
+    D = re.findall(r':::: {.columns}(.*?)::::',text)
+    for rem in [D,A,B,C]:
+        for i,a in enumerate(rem):
+            if isinstance(a, tuple):
+                for part in a:
+                    text=text.replace(part,'')
+            else:
+                text=text.replace(a,'')
+    text=text.replace('.r .cell-code','r')
+    text=text.replace('::: {.cell}','')
+    text=text.replace(':::: {.columns}','')
+    text=text.replace('::: {.cell-output .cell-output-stdout}','')
+    text=text.replace('::: {.cell-output .cell-output-stderr}','')
+    text=text.replace('::: {.cell-output-display}','')
+    text=text.replace('::::','')
+    text=text.replace(':::','')
+    text=text.replace('```~~~~~~```','\n')
+    text=text.replace('~~~~~~','\n')
+    text=text.replace('#| include: false','')
+    text=text.replace('\n\n\n\n','\n\n')
+    return (text)
 
-# Home = os.getenv("QUARTO_PROJECT_OUTPUT_DIR")
+def nb_Rep(nbin,nbout):
+    
+    nb = nbformat.read(nbin,as_version=4)
+    drop_cells = []
+    for i, c in enumerate(nb.cells):
+        if c['cell_type'] == 'raw':
+            drop_cells.append(i)
+        elif c['cell_type'] == 'markdown':
+            if ':::: {.columns}' in c['source']:
+                drop_cells.append(i)
+        elif c['cell_type'] == 'code':
+            c['source'] = c['source'].replace('#| include: false','')
 
-# for folder in ['Assignments']:
-#     PDF_Dir = f"{Home}/{folder}"
-#     Sheet_Dir = f"{folder}/_answer_keys"
-#     Code_Dir = f"{folder}/_answer_keys"
-#     OutDir = f"{KeyDir}/{folder}"
-#     if not os.path.exists(OutDir):
-#         os.mkdir(OutDir)
-#     for file in os.listdir(PDF_Dir):
-#         if file.endswith(".pdf"):
-#             shutil.move(f"{PDF_Dir}/{file}",f"{OutDir}/{file}")
-#     for file in os.listdir(Code_Dir):
-#         if file.endswith(".R"):
-#             shutil.move(f"{Code_Dir}/{file}",f"{OutDir}/{file}")
-#     for file in os.listdir(Sheet_Dir):
-#         if file.startswith("Lab"):
-#             L = file.split('_')[0]
-#             MarkingSheet = pd.read_csv(f"{Sheet_Dir}/MarkingSheet_{L}.csv",index_col='Question')
-#             Answers = pd.read_csv(f"{Sheet_Dir}/{L}_AnswerList.csv",index_col='Question')
-#             Key = MarkingSheet.join(Answers)
-#             Key.to_csv(f"{OutDir}/MarkingSheet_{L}.csv")
-#         elif file.endswith('.zip'):
-#             shutil.move(f"{Sheet_Dir}/{file}",f"{OutDir}/{file}")
+    for i in drop_cells[::-1]:
+        nb.cells.pop(i)
+
+    nbformat.write(nb,  nbout)
+
+Write_Dir = '../Assignment'
+Out_Dir = 'docs/'
+for In_Dir in ['Assignments/','AssignmentsR_out/']:
+    for file in os.listdir(In_Dir):
+        mv = 0
+        Write_Out = f'{Write_Dir}{file.split(".")[0]}/'
+        if file.endswith(".html.md"):
+            with open(f"{In_Dir}/{file}",'r',encoding='utf-8') as T:
+                # text = T.read()
+                text = R_rep(T.read())
+            if file.startswith('_'):
+                fn = file[1:].split(".html.md")[0]
+            else:
+                fn = file.split(".html.md")[0]
+            with open(Write_Out+fn+'.Rmd','w',encoding='utf-8') as out:
+                out.write(text)
+            # os.remove(f"{In_Dir}/{file}")
+            print(f'Converted {file} to RMD')
+            mv = 1
+        elif file.endswith(".ipynb"):
+            nb_Rep(In_Dir+file, Write_Out+file)
+            # shutil.move(In_Dir+file, Write_Out+file)
+
+            # os.remove(f"{In_Dir}/{file}")
+            mv = 1
+            print(f'Moved {file}')
+            
--- _Code/MakeR.py
+++ _Code/MakeR.py
@@ -1,8 +1,11 @@
-import os, shutil
-import re
+import os
+
+# if not os.getenv("QUARTO_PROJECT_RENDER_ALL"):
+#     exit()
+
 
 In_Dir = 'Assignments/'
-# Out_Dir = '../AssignmentCode/'
+Out_Dir = 'AssignmentsR_out/'
 
 py_head = '''
 jupyter: python3
@@ -22,60 +25,14 @@ R_code = '_R.'
 
 print(In_Dir)
 for file in os.listdir(In_Dir):
-    if file.endswith("01_py.qmd"):
+    if file.endswith(".qmd") and file.startswith('index') == False:
         with open(f"{In_Dir}/{file}",'r',encoding='utf-8') as T:
 
             text = T.read().replace(py_head,R_head)
             print(text[:100])
             text = text.replace(py_code,R_code)
-            # A = re.findall(r'::: {.cell-output .cell-output-stdout}(.*?):::',text)
-            # for i,a in enumerate(A):
-            #     text=text.replace(a,'\n')
-            # text=text.replace('.r','r')
-            # text=text.replace('::: {.cell}','\n')
-            # text=text.replace('::: {.cell-output .cell-output-stdout}','\n')
-            # text=text.replace(':::','\n')
-            # text=text.replace('~~~~~~','\n')
+            text = text.replace(' _Includes','../Assignments/_Includes')
             
-        with open(In_Dir+file.split("_py")[0]+'_R.qmd','w',encoding='utf-8') as out:
+        with open(Out_Dir+file,'w',encoding='utf-8') as out:
             out.write(text)
         print(f'Converted {file} to RMD')
-    # elif file.endswith(".ipynb"):
-    #     shutil.copyfile(In_Dir+file, Out_Dir+file)
-
-    
-    #     print(f'Moved {file}')
-
-    
-
-# import pandas as pd
-
-# if not os.getenv("QUARTO_PROJECT_RENDER_ALL"):
-#     exit()
-
-# KeyDir = '../AnswerKeys'
-
-# Home = os.getenv("QUARTO_PROJECT_OUTPUT_DIR")
-
-# for folder in ['Assignments']:
-#     PDF_Dir = f"{Home}/{folder}"
-#     Sheet_Dir = f"{folder}/_answer_keys"
-#     Code_Dir = f"{folder}/_answer_keys"
-#     OutDir = f"{KeyDir}/{folder}"
-#     if not os.path.exists(OutDir):
-#         os.mkdir(OutDir)
-#     for file in os.listdir(PDF_Dir):
-#         if file.endswith(".pdf"):
-#             shutil.move(f"{PDF_Dir}/{file}",f"{OutDir}/{file}")
-#     for file in os.listdir(Code_Dir):
-#         if file.endswith(".R"):
-#             shutil.move(f"{Code_Dir}/{file}",f"{OutDir}/{file}")
-#     for file in os.listdir(Sheet_Dir):
-#         if file.startswith("Lab"):
-#             L = file.split('_')[0]
-#             MarkingSheet = pd.read_csv(f"{Sheet_Dir}/MarkingSheet_{L}.csv",index_col='Question')
-#             Answers = pd.read_csv(f"{Sheet_Dir}/{L}_AnswerList.csv",index_col='Question')
-#             Key = MarkingSheet.join(Answers)
-#             Key.to_csv(f"{OutDir}/MarkingSheet_{L}.csv")
-#         elif file.endswith('.zip'):
-#             shutil.move(f"{Sheet_Dir}/{file}",f"{OutDir}/{file}")
